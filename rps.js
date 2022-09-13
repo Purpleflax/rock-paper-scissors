ea68328b07524b76ea2a71049d05d2a9e529cf2b
Purpleflax <reynoldsjeffrey414@gmail.com>
2022-09-13 16:59:44 +0100

    Functional rounds

--- rps.js
+++ rps.js
@@ -1,27 +1,36 @@
-let array = [1, 2, 3];
-let rand = Math.random()
-rand *= array.lenght;
-rand = Math.floor(rand);
+
 
 // 1 = Rock
 // 2 = Paper
 // 3 = Scissors
-let playerChoiceString = prompt("Please type Rock, Paper, or Scissors.")
 
-if (playerChoiceString == "Rock") {
-    playerChoice == 1;
-}
-    else if (playerChoiceString == "Paper") {
-    playerChoice == 2;
+let playerChoice = 0
+let playerScore = 0
+let computerScore = 0
+let computerChoice = 0
+
+
+
+
+
+
+function playRound() {
+
+    computerChoice = Math.floor(Math.random() * 3) + 1
+    let playerChoiceString1 = prompt("Please type Rock, Paper, or Scissors.")
+    let playerChoiceString = playerChoiceString1.slice(0).toLowerCase();
+    if (playerChoiceString == "rock") {
+    (playerChoice = 1)
     }
-    else if (playerChoiceString == "Scissors") {
-    playerChoice == 3;
+    else if (playerChoiceString == "paper") {
+    (playerChoice = 2)
+    }
+    else if (playerChoiceString == "scissors") {
+    (playerChoice = 3)
     }
     else {
     console.log("You have chosen an invalid input.")
     }
-
-function playRound(playerChoice, computerChoice) {
     if (playerChoice == computerChoice) {
         return("Draw")
     }
@@ -29,28 +38,47 @@ function playRound(playerChoice, computerChoice) {
     
     if (playerChoice == 1) {
         if (computerChoice == 2) {
-            return("Computer Wins")
+            computerScore++
+            return("Computer Wins! Paper beats Rock.")
         }   else if(computerChoice == 3) {
-            return("Player Wins")
+            playerScore++
+            return("Player Wins! Rock beats Scissors.")
         }
     }
     //Rock selection function
 
     if (playerChoice == 2) {
         if (computerChoice == 3) {
-            return("Computer Wins")
+            computerScore++
+            return("Computer Wins! Scissors beats Paper.")
         }   else if(computerChoice == 1) {
-            return("Player Wins")
+            playerScore++
+            return("Player Wins! Paper beats Rock.")
         }
     }
     //Paper selection function
 
     if (playerChoice == 3) {
         if (computerChoice == 1) {
-            return("Computer Wins")
+            computerScore++
+            return("Computer Wins! Rock beats Scissors.")
         }   else if(computerChoice == 2) {
-            return("Player Wins")
+            playerScore++
+            return("Player Wins! Scissors beats Paper.")
         }
     }
     //Scissors selection function
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound()
+    }
+}
+
+if (playerScore > computerScore) {
+    console.log("Player Wins!")
+} 
+else if (playerScore < computerScore) {
+    console.log("Computer Wins!")
 }
